83493e82a5924974638525c63762159383a407fb
Abdur-rasheedidris19 <idrisrash2017@gmail.com>
2020-09-11 20:09:16 +0100

    second commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function() {
+//Preloader
+preloaderFadeOutTime = 200;
+function hidePreloader() {
+var preloader = $('.spinner-wrapper');
+preloader.fadeOut(preloaderFadeOutTime);
+}
+hidePreloader();
+});
